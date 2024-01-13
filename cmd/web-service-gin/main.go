4fb0e2a17f392ed050bf39ea7cd93a5b21c3cd1b
Ryan Kline <ryanjkline@protonmail.com>
2024-01-13 18:49:11 -0500

    Connects Postgres DB

--- cmd/web-service-gin/main.go
+++ cmd/web-service-gin/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"example/web-service-gin/data"
 	"example/web-service-gin/internal/handlers"
 	"example/web-service-gin/internal/routes"
 	"example/web-service-gin/internal/services"
 )
 
 func main() {
-    albumService := &services.AlbumService{}
+    dbConnection, err := data.ConnectToDatabase()
+    if err != nil {
+        panic(err)
+    }
+    defer dbConnection.Close()
+
+    albumService := &services.AlbumService{
+        DatabaseConnection: dbConnection,
+    }
 
     albumHandler := &handlers.AlbumHandler{
         AlbumService: albumService,
